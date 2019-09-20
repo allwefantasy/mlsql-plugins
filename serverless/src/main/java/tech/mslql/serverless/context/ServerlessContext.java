8ef9ca762f142444aa4f546c574a1c8d35b1414e
WilliamZhu <allwefantasy@gmail.com>
2019-09-20 15:07:03 +0800

    add plugin stream-boostrap-at-startup

---
+++ serverless/src/main/java/tech/mslql/serverless/context/ServerlessContext.java
@@ -0,0 +1,17 @@
+package tech.mslql.serverless.context;
+
+/**
+ * 2019-09-19 WilliamZhu(allwefantasy@gmail.com)
+ */
+public class ServerlessContext {
+//    LoadingCache<Table, MysqlClient> graphs = CacheBuilder.newBuilder()
+//            .maximumSize(1000)
+//            .expireAfterWrite(10, TimeUnit.MINUTES)
+//            .removalListener(MY_LISTENER)
+//            .build(
+//                    new CacheLoader<Key, Graph>() {
+//                        public Graph load(Key key) throws AnyException {
+//                            return createExpensiveGraph(key);
+//                        }
+//                    });
+}
---
+++ stream-boostrap-at-startup/src/main/java/tech/mlsq/streambootstrapatstartup/StreamApp.scala
@@ -0,0 +1,58 @@
+package tech.mlsq.streambootstrapatstartup
+
+import org.apache.spark.sql.SparkSession
+import streaming.core.strategy.platform.{PlatformManager, SparkRuntime}
+import streaming.dsl.{MLSQLExecuteContext, ScriptSQLExec, ScriptSQLExecListener}
+import tech.mlsql.ets.{SchedulerCommand, ScriptRunner}
+import tech.mlsql.job.{JobManager, MLSQLJobType}
+import tech.mlsql.version.VersionCompatibility
+
+/**
+  * 2019-09-20 WilliamZhu(allwefantasy@gmail.com)
+  */
+class StreamApp extends tech.mlsql.app.App with VersionCompatibility {
+
+
+  override def run(args: Seq[String]): Unit = {
+    val root = runtime.sparkSession
+    import SchedulerCommand._
+    import root.implicits._
+
+    val streams = tryReadTable(root, StreamAppConfig.TABLE, () => root.createDataset[Stream](Seq()).toDF())
+    streams.as[Stream].collect().foreach { stream =>
+      val session = getSessionByOwner(stream.owner)
+      val job = JobManager.getJobInfo(stream.owner, stream.name, MLSQLJobType.SCRIPT, stream.content, -1)
+      setUpScriptSQLExecListener(stream.owner, session, job.groupId, stream.home)
+      ScriptRunner.runJob(stream.content, job, (df) => {
+
+      })
+    }
+  }
+
+  def setUpScriptSQLExecListener(owner: String, sparkSession: SparkSession, groupId: String, home: String) = {
+    val context = new ScriptSQLExecListener(sparkSession, "", Map[String, String](owner -> home))
+    ScriptSQLExec.setContext(new MLSQLExecuteContext(context, owner, context.pathPrefix(None), groupId, Map()))
+    context.addEnv("SKIP_AUTH", "true")
+    context.addEnv("HOME", context.pathPrefix(None))
+    context.addEnv("OWNER", owner)
+    context
+  }
+
+  def getSessionByOwner(owner: String) = {
+    runtime.getSession(owner)
+  }
+
+  def runtime = {
+    PlatformManager.getRuntime.asInstanceOf[SparkRuntime]
+  }
+
+  override def supportedVersions: Seq[String] = {
+    Seq("1.5.0-SNAPSHOT", "1.5.0")
+  }
+}
+
+object StreamAppConfig {
+  val TABLE = "__mlsql__.streambootstrapatstartup_streams"
+}
+
+case class Stream(name: String, content: String, owner: String, home: String)
