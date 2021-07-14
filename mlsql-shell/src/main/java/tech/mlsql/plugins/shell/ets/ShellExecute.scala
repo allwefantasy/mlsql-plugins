fa96f6ee279453d31a66c113f40d05dde4595080
WilliamZhu <allwefantasy@gmail.com>
2021-07-14 10:35:23 +0800

    update

--- mlsql-shell/src/main/java/tech/mlsql/plugins/shell/ets/ShellExecute.scala
+++ mlsql-shell/src/main/java/tech/mlsql/plugins/shell/ets/ShellExecute.scala
@@ -8,6 +8,7 @@ import streaming.dsl.mmlib.SQLAlg
 import streaming.dsl.mmlib.algs.Functions
 import streaming.dsl.mmlib.algs.param.{BaseParams, WowParams}
 import tech.mlsql.common.utils.serder.json.JSONTool
+import tech.mlsql.common.utils.shell.ShellCommand
 import tech.mlsql.dsl.auth.ETAuth
 import tech.mlsql.dsl.auth.dsl.mmlib.ETMethod.ETMethod
 import tech.mlsql.plugins.shell.app.MLSQLShell
@@ -27,28 +28,37 @@ class ShellExecute(override val uid: String) extends SQLAlg
    */
   override def train(df: DataFrame, path: String, params: Map[String, String]): DataFrame = {
     val args = JSONTool.parseJson[List[String]](params("parameters"))
-    val process = os.proc(args).spawn()
-    val result = ArrayBuffer[String]()
-
-    var errLine = process.stderr.readLine()
-
-    while (errLine != null) {
-      logInfo(format(errLine))
-      result.append(errLine)
-      errLine = process.stderr.readLine()
-    }
-
-
-    var line = process.stdout.readLine()
-    while (line != null) {
-      logInfo(format(line))
-      result.append(line)
-      line = process.stdout.readLine()
-    }
-
-
     import df.sparkSession.implicits._
-    df.sparkSession.createDataset[String](result.toSeq).toDF("content")
+
+    args.head match {
+      case "script" =>
+        val res = ShellCommand.exec(args.last)
+        df.sparkSession.createDataset[String](Seq(res)).toDF("content")
+      case _ =>
+
+        val process = os.proc(args).spawn()
+        val result = ArrayBuffer[String]()
+
+        var errLine = process.stderr.readLine()
+
+        while (errLine != null) {
+          logInfo(format(errLine))
+          result.append(errLine)
+          errLine = process.stderr.readLine()
+        }
+
+
+        var line = process.stdout.readLine()
+        while (line != null) {
+          logInfo(format(line))
+          result.append(line)
+          line = process.stdout.readLine()
+        }
+
+        df.sparkSession.createDataset[String](result.toSeq).toDF("content")
+    }
+
+
   }
 
   override def skipPathPrefix: Boolean = false
