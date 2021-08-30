24dce6aa0d263d47ad0fb7d50db63b869db7b3cd
WilliamZhu <allwefantasy@gmail.com>
2021-08-30 16:28:12 +0800

    udpate

--- mlsql-language-server/src/main/java/tech/mlsql/plugins/langserver/MLSQLLanguageServer.java
+++ mlsql-language-server/src/main/java/tech/mlsql/plugins/langserver/MLSQLLanguageServer.java
@@ -57,6 +57,7 @@ public class MLSQLLanguageServer implements LanguageServer, Endpoint, JsonRpcMet
             }
             exit();
         });
+        stopThread.setDaemon(true);
         stopThread.start();
         return CompletableFuture.supplyAsync(Object::new);
     }
