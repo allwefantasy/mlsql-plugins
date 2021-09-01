154109f9a7553a904b4cd8eb9e6c32fcc51ee117
WilliamZhu <allwefantasy@gmail.com>
2021-09-01 19:58:30 +0800

    update

--- mlsql-language-server/src/main/java/tech/mlsql/plugins/langserver/AutoSuggestWrapper.scala
+++ mlsql-language-server/src/main/java/tech/mlsql/plugins/langserver/AutoSuggestWrapper.scala
@@ -2,6 +2,7 @@ package tech.mlsql.plugins.langserver
 
 import tech.mlsql.autosuggest.app.AutoSuggestController
 import tech.mlsql.autosuggest.statement.SuggestItem
+import tech.mlsql.common.utils.log.Logging
 import tech.mlsql.common.utils.serder.json.JSONTool
 
 import scala.collection.JavaConverters._
@@ -9,10 +10,18 @@ import scala.collection.JavaConverters._
 /**
  * 1/9/2021 WilliamZhu(allwefantasy@gmail.com)
  */
-class AutoSuggestWrapper(params: java.util.Map[String, String]) {
+class AutoSuggestWrapper(params: java.util.Map[String, String]) extends Logging {
   def run() = {
-    val suggest = new AutoSuggestController();
-    val jsonStr = suggest.run(params.asScala.toMap)
-    JSONTool.parseJson[List[SuggestItem]](jsonStr).asJava
+    try {
+      val suggest = new AutoSuggestController();
+      val jsonStr = suggest.run(params.asScala.toMap)
+      JSONTool.parseJson[List[SuggestItem]](jsonStr).asJava
+    } catch {
+      case e: Exception =>
+        logInfo("Suggest fail", e)
+        List[SuggestItem]().asJava
+    }
+
+
   }
 }
