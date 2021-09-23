f40725d9778264daa1245ed0d2784f0536fe0e09
WilliamZhu <allwefantasy@gmail.com>
2021-09-23 14:36:40 +0800

    update

--- install-all.sh
+++ install-all.sh
@@ -2,10 +2,15 @@
 for spark_version in spark243 spark311
 do
 ./install.sh mlsql-shell ${spark_version}
+./install.sh mlsql-assert ${spark_version}
 ./install.sh mlsql-mllib ${spark_version}
-./install.sh connect-persist ${spark_version}
-./install.sh last-command ${spark_version}
 ./install.sh mlsql-excel ${spark_version}
+
+
+./install.sh connect-persist ${spark_version}
+
+
+./install.sh last-command ${spark_version}
 ./install.sh run-script ${spark_version}
 ./install.sh save-then-load ${spark_version}
 ./install.sh stream-persist ${spark_version}
