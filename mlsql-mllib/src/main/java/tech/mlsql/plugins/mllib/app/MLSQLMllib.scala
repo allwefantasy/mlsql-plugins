45b35ca3498ea9135ab1d38a55986d56a9780720
WilliamZhu <allwefantasy@gmail.com>
2021-10-10 09:44:58 +0800

    update

--- mlsql-mllib/src/main/java/tech/mlsql/plugins/mllib/app/MLSQLMllib.scala
+++ mlsql-mllib/src/main/java/tech/mlsql/plugins/mllib/app/MLSQLMllib.scala
@@ -1,8 +1,9 @@
 package tech.mlsql.plugins.mllib.app
 
 import tech.mlsql.common.utils.log.Logging
+import tech.mlsql.dsl.CommandCollection
 import tech.mlsql.ets.register.ETRegister
-import tech.mlsql.plugins.mllib.ets.{AutoMLExt, ClassificationEvaluator, RegressionEvaluator, SampleDatasetExt, TakeRandomSampleExt}
+import tech.mlsql.plugins.mllib.ets._
 import tech.mlsql.version.VersionCompatibility
 
 /**
@@ -15,6 +16,14 @@ class MLSQLMllib extends tech.mlsql.app.App with VersionCompatibility with Loggi
     ETRegister.register("AutoMLExt", classOf[AutoMLExt].getName)
     ETRegister.register("SampleDatasetExt", classOf[SampleDatasetExt].getName)
     ETRegister.register("TakeRandomSampleExt", classOf[TakeRandomSampleExt].getName)
+    ETRegister.register("ColumnsExt", classOf[ColumnsExt].getName)
+
+    // !columns drop fields from tableName;
+    CommandCollection.refreshCommandMapping(Map("columns" ->
+      """
+        |run {3} as ColumnsExt.`` where action="{0}" and fields="{1}"
+        |""".stripMargin))
+
   }
 
 
---
+++ mlsql-mllib/src/main/java/tech/mlsql/plugins/mllib/ets/ColumnsExt.scala
@@ -0,0 +1,62 @@
+package tech.mlsql.plugins.mllib.ets
+
+import org.apache.spark.ml.param.{Param, StringArrayParam}
+import org.apache.spark.sql.expressions.UserDefinedFunction
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import streaming.dsl.mmlib.{Code, Doc, MarkDownDoc, ModelType, ProcessType, SQLAlg, SQLCode}
+import streaming.dsl.mmlib.algs.classfication.BaseClassification
+import streaming.dsl.mmlib.algs.{Functions, MllibFunctions}
+
+/**
+ * 10/10/2021 WilliamZhu(allwefantasy@gmail.com)
+ */
+class ColumnsExt(override val uid: String) extends SQLAlg
+  with Functions
+  with MllibFunctions
+  with BaseClassification
+  with PluginBaseETAuth {
+
+  override def train(df: DataFrame, path: String, params: Map[String, String]): DataFrame = {
+    val _action = params.getOrElse(action.name, $(action).toString)
+    val _fields = params.getOrElse(fields.name, $(fields).mkString(",")).split(",")
+    if (_fields.length == 0) return df
+    _action match {
+      case "drop" =>
+        df.drop(_fields: _*)
+    }
+  }
+
+  override def batchPredict(df: DataFrame, path: String, params: Map[String, String]): DataFrame = {
+    train(df, path, params)
+  }
+
+  override def modelType: ModelType = ProcessType
+
+  override def doc: Doc = Doc(MarkDownDoc,
+    """
+      |
+      |""".stripMargin)
+
+  override def codeExample: Code = Code(SQLCode,
+    """
+      |select 1 as a,2 as b as mockTable;
+      |!columns drop a from mockTable;
+      |select * from mockTable as output;
+      |""".stripMargin)
+
+
+  override def load(sparkSession: SparkSession, path: String, params: Map[String, String]): Any = ???
+
+  override def predict(sparkSession: SparkSession, _model: Any, name: String, params: Map[String, String]): UserDefinedFunction = ???
+
+  override def etName: String  = "__columns_operator__"
+
+  final val action: Param[String] =
+    new Param[String](this, name = "action", doc = "")
+  setDefault(action, "drop")
+
+  final val fields: StringArrayParam =
+    new StringArrayParam(this, name = "fields", doc = "")
+  setDefault(fields, Array[String]())
+
+}
