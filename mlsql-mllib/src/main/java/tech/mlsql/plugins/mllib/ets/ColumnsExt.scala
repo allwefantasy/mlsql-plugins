4733e194129249ee6df91780ad6f5ef306903faf
WilliamZhu <allwefantasy@gmail.com>
2021-10-10 10:04:33 +0800

    update

--- mlsql-mllib/src/main/java/tech/mlsql/plugins/mllib/ets/ColumnsExt.scala
+++ mlsql-mllib/src/main/java/tech/mlsql/plugins/mllib/ets/ColumnsExt.scala
@@ -3,9 +3,10 @@ package tech.mlsql.plugins.mllib.ets
 import org.apache.spark.ml.param.{Param, StringArrayParam}
 import org.apache.spark.sql.expressions.UserDefinedFunction
 import org.apache.spark.sql.{DataFrame, SparkSession}
-import streaming.dsl.mmlib.{Code, Doc, MarkDownDoc, ModelType, ProcessType, SQLAlg, SQLCode}
 import streaming.dsl.mmlib.algs.classfication.BaseClassification
+import streaming.dsl.mmlib.algs.param.BaseParams
 import streaming.dsl.mmlib.algs.{Functions, MllibFunctions}
+import streaming.dsl.mmlib._
 
 /**
  * 10/10/2021 WilliamZhu(allwefantasy@gmail.com)
@@ -16,15 +17,23 @@ class ColumnsExt(override val uid: String) extends SQLAlg
   with BaseClassification
   with PluginBaseETAuth {
 
+  def this() = this(BaseParams.randomUID())
+
   override def train(df: DataFrame, path: String, params: Map[String, String]): DataFrame = {
     val _action = params.getOrElse(action.name, $(action).toString)
     val _fields = params.getOrElse(fields.name, $(fields).mkString(",")).split(",")
+    val dfName = params("__dfname__")
     if (_fields.length == 0) return df
     _action match {
       case "drop" =>
-        df.drop(_fields: _*)
+        val newdf = df.drop(_fields: _*)
+        newdf.createOrReplaceTempView(dfName)
+        newdf
     }
   }
+
+
+  override def skipOriginalDFName: Boolean = false
 
   override def batchPredict(df: DataFrame, path: String, params: Map[String, String]): DataFrame = {
     train(df, path, params)
@@ -49,7 +58,7 @@ class ColumnsExt(override val uid: String) extends SQLAlg
 
   override def predict(sparkSession: SparkSession, _model: Any, name: String, params: Map[String, String]): UserDefinedFunction = ???
 
-  override def etName: String  = "__columns_operator__"
+  override def etName: String = "__columns_operator__"
 
   final val action: Param[String] =
     new Param[String](this, name = "action", doc = "")
